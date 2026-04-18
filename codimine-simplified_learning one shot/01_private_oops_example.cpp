7e1e894d9ada2bee19a326244a83226c40c76045
keshav verma <keshavcode1@gmail.com>
2026-04-18 12:53:58 +0530

    01-private-oops example

---
+++ codimine-simplified_learning one shot/01_private_oops_example.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+// class 
+
+class house{
+    public:
+    // member variables
+
+
+    int l,b;
+
+    // member functions 
+
+
+    void setData(int x , int y){
+        l=x;
+        b=y;
+    }
+    void area(){
+        cout<<l*b;
+    }
+};
+int main(){
+    house h1 ; // object created
+    h1.setData(20,30);
+    h1.area();
+
+    return 0;
+}
